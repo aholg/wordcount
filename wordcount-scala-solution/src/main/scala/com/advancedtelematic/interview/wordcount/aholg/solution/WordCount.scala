db9a41ef21896cabe73bd650d55eddd295915e97
aholg <holgersson31@hotmail.com>
2021-07-21 23:30:39 +0200

    Run 10 instances of SlowCharacterReader

--- wordcount-scala-solution/src/main/scala/com/advancedtelematic/interview/wordcount/aholg/solution/WordCount.scala
+++ wordcount-scala-solution/src/main/scala/com/advancedtelematic/interview/wordcount/aholg/solution/WordCount.scala
@@ -4,54 +4,101 @@ import java.io.EOFException
 
 import akka.NotUsed
 import akka.actor.ActorSystem
-import akka.stream.scaladsl.{Concat, Framing, Sink, Source}
+import akka.stream.scaladsl.{Framing, Sink, Source}
 import akka.util.ByteString
-import com.advancedtelematic.interview.wordcount.{CharacterReader, FastCharacterReaderImpl}
+import com.advancedtelematic.interview.wordcount.{CharacterReader, FastCharacterReaderImpl, SlowCharacterReaderImpl}
 
-import scala.collection.immutable
+import scala.collection.mutable
+import scala.concurrent.duration.DurationInt
 import scala.concurrent.{ExecutionContext, Future}
 
 object Main extends App {
   private implicit val system: ActorSystem = ActorSystem("WordCount")
   private implicit val ec: ExecutionContext = system.dispatcher
 
-  private val wordCounter = new WordCount(new FastCharacterReaderImpl())
+  private val fastWordCounter = new WordCount(Seq(new FastCharacterReaderImpl()))
+  private val slowWordCounter = new WordCount(Seq.fill(10)(new SlowCharacterReaderImpl()))
 
-  wordCounter
+  private val fastCounter: Future[Seq[Unit]] = fastWordCounter
     .process
-    .map(_.map(r => println(s"${r._1} - ${r._2}")))
-    .flatMap(_ => system.terminate())
+    .map { res =>
+      println("Fast final wordcount:")
+      res.map(r => println(s"${r._1} - ${r._2}"))
+    }
+
+  private val slowCounter: Future[Seq[Unit]] = slowWordCounter
+    .process
+    .map { res =>
+      println("Slow final wordcount:")
+      res.map(r => println(s"${r._1} - ${r._2}"))
+    }
+  for {
+    _ <- fastCounter
+    _ <- slowCounter
+    s <- system.terminate()
+  } yield s
 }
 
-class WordCount(characterReader: CharacterReader)(implicit ex: ExecutionContext, actorSystem: ActorSystem) {
+class WordCount(characterReaders: Seq[CharacterReader])
+               (implicit ex: ExecutionContext, actorSystem: ActorSystem) {
 
-  private val charIterator: Iterator[Char] = {
-    Iterator.continually {
-      val c = characterReader.nextCharacter()
-      if (c == '\n') ' '
-      else c
-    }
-  }
-
-  private val forbiddenCharactersRegex = "('\\s|\\s'|[;,]|^'|'$|\\.)"
+  private val forbiddenCharactersRegex = "('\\s|\\s'|[;,:]|^'|'$|\\.)"
 
   def process: Future[Seq[(String, Int)]] = {
     source
-      .via(Framing.delimiter(ByteString(' '), 256, allowTruncation = true).map(_.utf8String.toLowerCase.replaceAll(forbiddenCharactersRegex, "")))
-      .runWith(Sink.seq[String])
-      .map(_.groupBy(identity).mapValues(_.size).toSeq)
-      .map(sortByOccurrencesDescending)
+      .statefulMapConcat(() => collectWords)
+      .groupedWithin(Int.MaxValue, 10.seconds)
+      .map{ ws =>
+        val latestCount = ws.lastOption.getOrElse(Map.empty)
+        sortByOccurrencesDescending(latestCount.toSeq)
+      }
+      .wireTap(printWordCountSummary(_))
+      .runWith(Sink.lastOption)
+      .map(_.getOrElse(Seq.empty))
   }
 
-  private def source: Source[ByteString, NotUsed] = {
-    Source
-      .fromIterator(() => charIterator)
-      .map(ByteString(_))
-      .recover {
+  private def collectWords: String => List[mutable.Map[String, Int]] = {
+    val wordOccurrences = mutable.Map.empty[String, Int]
+
+    word: String => {
+      wordOccurrences.update(word, wordOccurrences.getOrElse(word, 0) + 1)
+      wordOccurrences :: Nil
+    }
+  }
+
+  private def source: Source[String, NotUsed] = {
+    characterReaders
+      .map(cr =>
+        Source.fromIterator(() => charIterator(cr))
+          .map(c => ByteString(c))
+          .via(Framing.delimiter(ByteString(' '), 256, allowTruncation = true))
+      )
+      .reduce(_ merge _)
+      .map(w => stripNonWordCharacters(w.utf8String))
+      .async
+  }
+
+  private def charIterator(characterReader: CharacterReader): Iterator[Char] = {
+    Iterator.continually {
+      try {
+        val c = characterReader.nextCharacter()
+        if (c == '\n') ' '
+        else c
+      } catch {
         case _: EOFException =>
           characterReader.close()
-          ByteString("")
+          '\0'
       }
+    }.takeWhile(_ != '\0')
+  }
+
+  private def printWordCountSummary(tuples: Seq[(String, Int)]): Unit = {
+    println("Wordcount:")
+    tuples.foreach(r => println(s"${r._1} - ${r._2}"))
+  }
+
+  private def stripNonWordCharacters(word: String): String = {
+    word.toLowerCase.replaceAll(forbiddenCharactersRegex, "")
   }
 
   private def sortByOccurrencesDescending(wordOccurrences: Seq[(String, Int)]): Seq[(String, Int)] = {
--- wordcount-scala-solution/src/test/scala/com/advancedtelematic/interview/aholg/solution/WordCountTest.scala
+++ wordcount-scala-solution/src/test/scala/com/advancedtelematic/interview/aholg/solution/WordCountTest.scala
@@ -2,13 +2,14 @@ package com.advancedtelematic.interview.aholg.solution
 
 import java.io.EOFException
 
-import akka.actor.ActorSystem
+import akka.actor.{ActorSystem, Scheduler}
 import akka.testkit.TestKit
 import com.advancedtelematic.interview.wordcount.CharacterReader
 import com.advancedtelematic.interview.wordcount.aholg.solution.WordCount
 import org.scalamock.scalatest.MockFactory
+import org.scalatest.BeforeAndAfterAll
 import org.scalatest.concurrent.ScalaFutures
-import org.scalatest.funsuite.{AnyFunSuite, AnyFunSuiteLike}
+import org.scalatest.funsuite.AnyFunSuiteLike
 import org.scalatest.matchers.should.Matchers
 
 class WordCountTest
@@ -16,15 +17,22 @@ class WordCountTest
   with AnyFunSuiteLike
   with Matchers
   with ScalaFutures
+  with BeforeAndAfterAll
   with MockFactory {
 
   import scala.concurrent.ExecutionContext.Implicits.global
+  implicit val scheduler: Scheduler = system.scheduler
+
+  override protected def afterAll(): Unit = {
+    TestKit.shutdownActorSystem(system)
+    super.afterAll()
+  }
 
   test("should return empty map for empty string") {
     val words = ""
     val reader: CharacterReader = testReader(words)
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res shouldBe Seq.empty
@@ -34,7 +42,7 @@ class WordCountTest
   test("should call close on end of stream") {
     val reader: CharacterReader = stub[CharacterReader]
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
     (reader.nextCharacter _).when().throws(new EOFException())
 
     whenReady(counter.process) { _ =>
@@ -45,7 +53,7 @@ class WordCountTest
   test("should return the count for a single character") {
     val reader: CharacterReader = testReader("A")
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res shouldBe Seq(("a", 1))
@@ -56,7 +64,7 @@ class WordCountTest
     val words = "word1\nword1\nword1"
     val reader: CharacterReader = testReader(words)
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res shouldBe Seq(("word1", 3))
@@ -67,7 +75,7 @@ class WordCountTest
     val words = "wordD\nwordA\nwordB\nwordC"
     val reader: CharacterReader = testReader(words)
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res should contain theSameElementsInOrderAs Map(
@@ -83,7 +91,7 @@ class WordCountTest
     val words = "wordD\nwordA\nwordB\nwordC\nwordD"
     val reader: CharacterReader = testReader(words)
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res should contain theSameElementsInOrderAs Seq(
@@ -99,7 +107,7 @@ class WordCountTest
     val words = "wordD wordA wordB\nwordC\nwordD"
     val reader: CharacterReader = testReader(words)
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res should contain theSameElementsInOrderAs Seq(
@@ -115,7 +123,7 @@ class WordCountTest
     val words = "wordD, wordA; wordB\nwordC\nwordD."
     val reader: CharacterReader = testReader(words)
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res should contain theSameElementsInOrderAs Seq(
@@ -131,7 +139,7 @@ class WordCountTest
     val words = "'wordD wordA word'B\nwordC\nwordD'"
     val reader: CharacterReader = testReader(words)
 
-    val counter = new WordCount(reader)
+    val counter = new WordCount(Seq(reader))
 
     whenReady(counter.process) { res =>
       res should contain theSameElementsInOrderAs Seq(
@@ -139,6 +147,21 @@ class WordCountTest
         ("word'b", 1),
         ("worda", 1),
         ("wordc", 1)
+      )
+    }
+  }
+
+  test("should be able to process 10 instances of a wordreader") {
+    val words = "'wordD wordA word'B\nwordC\nwordD'"
+
+    val counter = new WordCount(Seq.fill(10)(testReader(words)))
+
+    whenReady(counter.process) { res =>
+      res should contain theSameElementsInOrderAs Seq(
+        ("wordd", 20),
+        ("word'b", 10),
+        ("worda", 10),
+        ("wordc", 10)
       )
     }
   }
